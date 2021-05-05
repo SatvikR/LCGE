e8bc9ff5ba4757544fd475343792ff5437799bd8
Satvik Reddy <reddy.satvik@gmail.com>
2021-05-05 10:54:28 -0700

    added rectangle to input example

--- tests/input.c
+++ tests/input.c
@@ -24,6 +24,9 @@
 
 #include <stdio.h>
 
+#define WIN_WIDTH  800
+#define WIN_HEIGHT 800
+
 #define SPRITE_WIDTH  (326.0f * 0.75f)
 #define SPRITE_HEIGHT (296.0f * 0.75f)
 
@@ -37,7 +40,7 @@ int main(int argc, char const *argv[])
     }
 
     // Create a window
-    int success = lcge_create_context(800, 800,
+    int success = lcge_create_context(WIN_WIDTH, WIN_HEIGHT,
                                       "LCGE Input Example (Use arrow keys)",
                                       LCGE_NON_RESIZEABLE);
 
@@ -54,14 +57,16 @@ int main(int argc, char const *argv[])
     float speed = 10;
 
     LCGE_image *image = lcge_image_load("tests/images/player_sprite.png",
-                                        (800 - SPRITE_WIDTH) / 2,
-                                        (800 - SPRITE_HEIGHT) / 2,
+                                        (WIN_WIDTH - SPRITE_WIDTH) / 2,
+                                        (WIN_HEIGHT - SPRITE_HEIGHT) / 2,
                                         SPRITE_WIDTH, SPRITE_HEIGHT);
+    LCGE_rect *background = lcge_rect_load(0, 0, WIN_WIDTH, WIN_HEIGHT);
 
     while (lcge_window_is_open())
     {
         // Do any drawing here
         lcge_window_clear();
+        lcge_rect_draw(background, 29, 43, 83);
         float curr_x = lcge_image_get_x(image);
         float curr_y = lcge_image_get_y(image);
 
@@ -76,7 +81,7 @@ int main(int argc, char const *argv[])
         }
         if (lcge_get_key(LCGE_KEY_RIGHT) == LCGE_KEY_PRESSED)
         {
-            if ((curr_x + SPRITE_WIDTH) <= (800 - speed))
+            if ((curr_x + SPRITE_WIDTH) <= (WIN_WIDTH - speed))
             {
                 curr_x += speed;
                 lcge_image_set(image, curr_x, curr_y, SPRITE_WIDTH,
